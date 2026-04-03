3dc099f87b2cc2b856901686ebbc7e9269ffd54d
protongramzk <gunturnina05@gmail.com>
2026-04-03 17:29:01 +0800

    new api

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,6 @@
 // src/lib/auth.js
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://aipm-tawny.vercel.app'
 
 // ─────────────────────────────────────────
 // 🔓 JWT DECODE (PURE, TANPA LIB)
